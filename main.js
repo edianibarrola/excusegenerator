44f7aa0896d5cf24931add6598c9771ebf969422
edianibarrola <edianibarrola@gmail.com>
2020-12-02 21:00:45 +0000

    Update

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 let who = ['The dog ','My grandma ','His turtle ','My bird ']
 let action = ['ate ','peed on ','crushed ','broke ']
 let what = ['my homework ', 'the keys ', 'the car ', 'my computer ' ]
-let when = ['before the class ','right on time ','when I finished ','during my lunch ','while I was praying ']
+let when = ['before the class ','right on time ','when I finished ','while I was praying ']
 
 window.onload = () => {
     document.querySelector('#excuse').innerHTML = generateExcuse();
@@ -9,7 +9,7 @@ window.onload = () => {
 }
 
 let generateExcuse= () => {
-    console.log(who[randomNumber()] + action[randomNumber()] + what[randomNumber()] + when[randomNumber()]);
+    //console.log(who[randomNumber()] + action[randomNumber()] + what[randomNumber()] + when[randomNumber()]);
     return(who[randomNumber()] + action[randomNumber()] + what[randomNumber()] + when[randomNumber()]);
 };
 
